33e1cfe6911c6808857394105d01158df47e14ec
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-09 10:35:35 -0300

    finishing login controller method

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,9 +1,16 @@
 const { authUserByEmailAndPassword, getTokenJWT } = require('../services/loginService')
 
 exports.Login = async (req, res, next) => {
-    let email = req.body.email
-    let password = req.body.password
-    let auth = await authUserByEmailAndPassword(email, password)
-    console.log(auth)
-    res.send(await authUserByEmailAndPassword())
+    try {
+        let email = req.body.email
+        let password = req.body.password
+        let userStatus = await authUserByEmailAndPassword(email, password)
+        if (userStatus.auth === true) {
+            res.status(200).send({ status: 200, message: 'User succefully authenticated.', email: userStatus.email, token: getTokenJWT(email) })
+        } else {
+            res.status(401).send({ status: 401, message: 'User not authenticated.' })
+        }
+    } catch (err) {
+        return { message: 'There was an error while trying to auth user.', err }
+    }
 };
