86cd3ab86df913f4ec104708a14ae290a30c7ab4
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-09 10:11:01 -0300

    creating authUser method

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,9 +1,9 @@
-const getUserByEmailAndPassword = require('../services/loginService')
+const { authUserByEmailAndPassword, getTokenJWT } = require('../services/loginService')
 
 exports.Login = async (req, res, next) => {
     let email = req.body.email
     let password = req.body.password
-    let user = await getUserByEmailAndPassword(email, password)
-    console.log(user.length > 0 ? true : false)
-    res.send(await getUserByEmailAndPassword())
+    let auth = await authUserByEmailAndPassword(email, password)
+    console.log(auth)
+    res.send(await authUserByEmailAndPassword())
 };
--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,8 +1,24 @@
-module.exports = getUserByEmailAndPassword = async (email, password) => {
-    const connect = require('../config/databaseConnection')
-    const conn = await connect();
-    const sql = `select * from users where email = ? and password = ?`
-    const values = [email, password]
-    const [rows] = await conn.query(sql, values)
-    return await rows;
+module.exports = {
+    authUserByEmailAndPassword: async (email, password) => {
+        try {
+            const connect = require('../config/databaseConnection')
+            const bcrypt = require('bcrypt')
+            const conn = await connect();
+            const sql = `select * from users where email = ?`
+            const [rows] = await conn.query(sql, [email])
+            if (rows.length > 0) {
+                let user = rows[0]
+                if (await bcrypt.compare(password, user.password)) {
+                    return { auth: true }
+                }
+            }
+            return { auth: false }
+        } catch (err) {
+            return { message: 'There was an error while trying to auth user information.', err }
+        }
+    },
+
+    getTokenJWT: () => {
+
+    }
 }
